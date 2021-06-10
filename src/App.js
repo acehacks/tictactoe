022a2f433df54d4f77a747530994bdaf4607cdbe
acehacks <anuragsinghvt@gmail.com>
2021-06-10 07:38:52 +0530

    full hosted tictactoe game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Board from "./components/Board"
+import History from "./components/History"
+import StatusMessage from "./components/StatusMessage"
+import { calculateWinner } from './helper';
+// import "./Styles/style.css"
 
-function App() {
+const NEW_GAME =[
+    {board : Array(9).fill(null),isXNext : true}
+  ];
+
+const App = () =>  {
+  
+  const [history , setHistory] = useState(NEW_GAME);
+
+    const [currentMove,setCurrentMove] = useState(0);
+
+    const current=history[currentMove];
+    console.log('history',history);
+
+    const {winner,winningSquares} =calculateWinner(current.board);
+
+    // const message= winner ? `Winner is ${winner}` : `Next player is ${current.isXNext ? 'X' : 'O' }`;
+
+    const handleSquareClick = (position) => {
+        if (current.board[position] || winner){
+            return;
+        }
+        setHistory( prev  =>{
+
+          const last = prev[prev.length -1];
+
+          const newBoard = last.board.map((square,pos)=>{
+
+              if (position === pos) {
+                  return last.isXNext ? 'X':'O';
+              }
+              return square;
+          });
+          return prev.concat({board : newBoard , isXNext : !last.isXNext})
+        });
+        setCurrentMove(prev => prev +1);
+    }
+    const moveTo = (move)=>{
+      setCurrentMove(move);
+    }
+    const onNewGame = ()=>{
+      setHistory(NEW_GAME);
+      setCurrentMove(0);
+    }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <h1>TIC <span className="text-green"> TAC </span> TOE</h1>
+    <StatusMessage winner={winner} current={current}/>
+    <Board board={current.board} handleSquareClick={handleSquareClick} winningSquares={winningSquares}/>
+    <button onClick={onNewGame} className = {`btn-reset ${winner? 'active':'' }`}> Start New Game </button>
+    <h2 style={{fontWeight: 'normal'}}>Current game history</h2>
+    <History history={history} moveTo={moveTo} currentMove={currentMove}/>
+    <div className="bg-balls"/> 
     </div>
+     
   );
 }
 
